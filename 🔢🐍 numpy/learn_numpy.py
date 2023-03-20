9c2df66d26c1295bdb3b69d2ad0c551be3202549
ana thayna <ana.thayna@enjoei.com.br>
2023-03-19 21:00:28 -0300

    :construction: wip(numpy): organizing...

--- 🔢🐍 numpy/learn_numpy.py
+++ 🔢🐍 numpy/learn_numpy.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy import random
 
+########################### ARRAYS #############################
+
 a = np.array(42)
 b = np.array([1, 2, 3, 4, 5])
 c = np.array([[1, 2, 3], [4, 5, 6]])
@@ -12,13 +14,11 @@ print(d.ndim) # 3 dim
 print(c.shape) # (2, 3)
 print(d.shape) # (2,2,3)
 
-##############################################################
-
 x = [1,2,3]
 e = np.asarray(x)
 print(e)
 
-##############################################################
+########### object, string e timedelta e datetime ###########
 
 np.zeros(2) # array de 2 zeros
 np.ones(2) # array de 2 uns
@@ -27,19 +27,15 @@ np.empty(2) # conteúdo randômico, dependente na memória
 np.arange(4) # de 0 a 4
 np.linspace(0, 10, num=5) # de 0 a 10, divididos em 5 elementos
 
-##############################################################
+################## operações com elementos ##################
 
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print('5o elemento da primeira linha: ', arr[1, 4])
 arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 print(arr[0, 1, 2])
 
-##############################################################
-
 f = np.array([2, 1, 5, 3, 7, 4, 6, 8])
 np.sort(f) # cópia ordenanda
-
-##############################################################
 
 a = np.array([1, 2, 3, 4])
 b = np.array([5, 6, 7, 8])
@@ -53,19 +49,17 @@ x = np.array([[1, 2], [3, 4]])
 y = np.array([[5, 6], [7, 8]])
 np.concatenate((x, y), axis=0) # concatenar pelas colunas 
 
-##############################################################
-
 g = np.array([1,2,3])
 x = g.copy()
 
-##############################################################
+####################### index e slicing ######################
 
 data = np.array([1, 2, 3])
 data[1] # 2
 data[0:2] # array([1, 2])
 data[1:] # array([2, 3])
 
-##############################################################
+############################ busca ###########################
 
 a = np.array([[1 , 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 print(a[a < 5])
@@ -78,12 +72,10 @@ print(c)
 b = np.nonzero(a < 5)
 print(b)
 
-##############################################################
-
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 np.where(arr%2 == 1) # retorna apeas os índices
 
-##############################################################
+#################### operações com arrays ####################
 
 data = np.array([1, 2])
 ones = np.ones(2, dtype=int)
@@ -102,7 +94,7 @@ a = np.array([11, 11, 12, 13, 14, 15, 16, 17, 12, 13, 11, 14, 18, 19,
 20])
 print(np.unique(a))
 
-##############################################################
+###################### funções randômicas ####################
 
 x=random.randint(100, size=(5)) # 5 inteiros até 100
 x = random.rand(5) # 5 floats
@@ -115,7 +107,7 @@ print(x)
 
 x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100)) # p probabilidades, 100 itens
 
-##############################################################
+####################### Entrada e Saída ######################
 
 a = np.array([1,2,3,4,5])
 np.savetxt('learn_numpy_out.txt',a) # salva em uma arquivo com cada conteúdo em uma linha
